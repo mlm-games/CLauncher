1eacfdd59a7d828b0d02b907c42760c63a841e09
mlm-studios <you@example.com>
2025-04-03 11:49:48 +0530

    use new prefs here

--- app/src/main/java/app/clauncher/MainActivity.kt
+++ app/src/main/java/app/clauncher/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import app.clauncher.data.Constants
 import app.clauncher.data.Navigation
-import app.clauncher.data.Prefs
+import app.clauncher.data.PrefsDataStore
 import app.clauncher.helper.isDarkThemeOn
 import app.clauncher.helper.isEinkDisplay
 import app.clauncher.helper.isTablet
@@ -30,31 +30,42 @@ import app.clauncher.helper.setPlainWallpaper
 import app.clauncher.helper.showLauncherSelector
 import app.clauncher.ui.compose.CLauncherNavigation
 import app.clauncher.ui.theme.CLauncherTheme
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    private lateinit var prefs: Prefs
+    private lateinit var prefsDataStore: PrefsDataStore
     private lateinit var viewModel: MainViewModel
 
     override fun attachBaseContext(context: Context) {
         val newConfig = Configuration(context.resources.configuration)
-        newConfig.fontScale = Prefs(context).textSizeScale
-        applyOverrideConfiguration(newConfig)
+        //newConfig.fontScale = Prefs(context).textSizeScale
         super.attachBaseContext(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        prefs = Prefs(this)
-        if (isEinkDisplay()) prefs.appTheme = AppCompatDelegate.MODE_NIGHT_NO
-        AppCompatDelegate.setDefaultNightMode(prefs.appTheme)
+        prefsDataStore = PrefsDataStore(this)
+        lifecycleScope.launch {
+            val appTheme = prefsDataStore.appTheme.first()
+            if (isEinkDisplay()) {
+                prefsDataStore.setAppTheme(AppCompatDelegate.MODE_NIGHT_NO)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(appTheme)
+            }
+        }
+
         super.onCreate(savedInstanceState)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        if (prefs.firstOpen) {
-            viewModel.firstOpen(true)
-            prefs.firstOpen = false
-            prefs.firstOpenTime = System.currentTimeMillis()
+        lifecycleScope.launch {
+            val firstOpen = prefsDataStore.firstOpen.first()
+            if (firstOpen) {
+                viewModel.firstOpen(true)
+                prefsDataStore.setFirstOpen(false)
+                prefsDataStore.setFirstOpenTime(System.currentTimeMillis())
+            }
         }
 
         setupOrientation()
@@ -119,17 +130,26 @@ class MainActivity : ComponentActivity() {
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
-        AppCompatDelegate.setDefaultNightMode(prefs.appTheme)
-        if (prefs.plainWallpaper && AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
-            setPlainWallpaper()
-            recreate()
+        lifecycleScope.launch {
+            val appTheme = prefsDataStore.appTheme.first()
+            AppCompatDelegate.setDefaultNightMode(appTheme)
+
+            val plainWallpaper = prefsDataStore.plainWallpaper.first()
+            if (plainWallpaper && AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
+                setPlainWallpaper()
+                recreate()
+            }
         }
     }
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         when (result.resultCode) {
             RESULT_OK -> {
                 when (result.data?.getIntExtra("requestCode", 0)) { // Or however you're passing the request code back
-                    Constants.REQUEST_CODE_ENABLE_ADMIN -> prefs.lockModeOn = true
+                    Constants.REQUEST_CODE_ENABLE_ADMIN -> {
+                        lifecycleScope.launch {
+                            prefsDataStore.setLockMode(true)
+                        }
+                    }
                     Constants.REQUEST_CODE_LAUNCHER_SELECTOR -> resetLauncherViaFakeActivity()
                 }
             }
